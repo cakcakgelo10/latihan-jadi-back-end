461f9dec889c3ae2623eb65a928fd9cb5b495408
cakcakgelo10 <rezaardiansyah343@gmail.com>
2024-12-23 22:41:29 +0700

    Latihan jadi back-end hari ke'empat

---
+++ day-04/function.js
@@ -0,0 +1,20 @@
+// function
+function salam(nama) {
+    return `Halo, ${nama}!`;
+}
+console.log(salam("Reza")); // Output: Halo, Reza!
+// ===============================================
+
+// Function Expression
+const salamm = function (nama) {
+    return `Halo, ${nama}!`;
+};
+console.log(salamm("Reza")); // Output: Halo, Reza!
+// ================================================
+
+// Arrow function
+const salammm = (nama) => `Halo, ${nama}!`;
+console.log(salammm("Reza")); // Output: Halo, Reza!
+
+
+
