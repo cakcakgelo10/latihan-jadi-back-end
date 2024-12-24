d2a97ba9236fa7a2716c157fc96d7bbe560a5800
cakcakgelo10 <rezaardiansyah343@gmail.com>
2024-12-24 18:53:54 +0700

    Latihan jadi back-end hari ke'lima

---
+++ day-05/latihan/create_server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const waktu = new Date().toLocaleTimeString();
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end(`Halo waktu server saat ini: ${waktu}\n`);
+});
+
+server.listen(4000, () => {
+    console.log("Server berjalan di http://localhost:4000"); 
+});
